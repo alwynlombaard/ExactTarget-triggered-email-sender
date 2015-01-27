77522cbc965b35493be5205c069047c8422d3775
Daniel Wichmann <daniel.wichmann@justgiving.com>
2015-01-27 16:07:21 +0000

    Introduced ExactTarget exception types. Throwing a SubscriberExcludedException when ExactTarget returns an error code of 24 (subscriber excluded by list detective).

---
+++ ExactTarget.TriggeredEmail/Core/Exceptions/ExactTargetException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExactTarget.TriggeredEmail.Core.Exceptions
+{
+    public class ExactTargetException : Exception
+    {
+        public ExactTargetException()
+        {
+        }
+
+        public ExactTargetException(string message) : base(message)
+        {
+        }
+
+        public ExactTargetException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
